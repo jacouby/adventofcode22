004ff644aea378492d1983756c222dd38baaa5db
jacouby <jyeenope@gmail.com>
2022-12-02 17:33:09 +0000

    day 1 complete

--- 1/main.py
+++ 1/main.py
@@ -1,1 +1,14 @@
+input = open('input.txt').read().split('\n\n')
 
+totals = []
+
+for group in input:
+    total = 0
+    for num in group.split('\n'):
+        total = total + int(num)
+    totals.append(total)
+
+print(totals)
+print('\nSorted:')
+totals.sort(reverse=True)
+print(totals[0]+totals[1]+totals[2])
